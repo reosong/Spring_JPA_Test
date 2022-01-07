bb1a8a555d214596fd9a62fa156dd937ca885f83
reosong <ambition_1018@naver.com>
2022-01-07 15:01:36 +0900

    1,7 first commit

---
+++ src/test/java/jpa/jpashop2/Jpashop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.jpashop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpashop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
